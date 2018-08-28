df130ca07f4aae46233061dc933ed2d24204e12f
enomoto <kenta.enomoto@classi.jp>
2018-08-28 15:18:05 +0900

    Add code

---
+++ README.md
@@ -0,0 +1,12 @@
+# iOS Walkthrough Sample
+
+this is the walkthrough screen sample app with a storyboard, no code.
+
+# Created with
+
+Xcode 9.4.1
+
+# License
+
+MIT
+
---
+++ WalkthroughSample/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  WalkthroughSample
+//
+//  Created by kenta.enomoto on 2018/08/28.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
